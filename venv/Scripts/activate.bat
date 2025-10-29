f76be0e1c4c8c0d1648e6a6d8b1e5c8cd13cea85
Bibek <bibek.joshi91@gmail.com>
2025-10-29 14:52:47 +0545

    first

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\new_datascience\Nepali News Dataset\venv
+set VIRTUAL_ENV=D:\new_datascience\Tihar Assignment\Nepali News Dataset\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
